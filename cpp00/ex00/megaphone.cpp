3d1eab3364f8f71766e7c726e9324c3a96c19194
calamitymandy <amandined.plateforme@gmail.com>
2024-08-17 15:03:25 +0200

    yay first one working

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,6 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amdemuyn <amdemuyn@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/17 14:12:24 by amdemuyn          #+#    #+#             */
+/*   Updated: 2024/08/17 14:54:42 by amdemuyn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include <iostream>
 
-int main
+int main(int argc, char **argv)
 {
-	int i;
-{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)std::toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
