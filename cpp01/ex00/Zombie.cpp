233cae6c20044dc0e388dd9aadb198366f2a6f48
Amandine <amandined.plateforme@gmail.com>
2025-09-05 13:15:57 +0200

    CPP01-ex00 TESTING

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -14,8 +14,11 @@ Zombie::Zombie(std::string name) { // PARAMETER: "name"
 
 Zombie::~Zombie() {
     std::cout << this->name << " is destroyed." << std::endl;
+    //this->name = "💀 (invalid)";
 }
 
-void    Zombie::announce(void){
-    std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << strd::endl;
+void    Zombie::announce(void) {
+    if (this->name != "💀 (invalid)") {
+        std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+    } 
 }
--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -7,12 +7,35 @@
 int main() {
 
 	// Heap allocation
-	Zombie* heapZombie = newZombie("Heapster");
+	Zombie* heapZombie = newZombie("Heapheapheap hurrah");
 	heapZombie->announce();
 	delete heapZombie; // Must delete to avoid memory leaks
 	
 	// Stack allocation
-	randomChump("Stacky");
+	randomChump("Stackatakata");
+
+	// Undefined behavior: trying to use heapZombie pointer after destruction
+	std::cout << "Undefined behavior: trying to use heapZombie pointer after destruction" << std::endl;
+	heapZombie->announce();
+	std::cout << std::endl;
 	
 	return (0);
 }
+
+/*
+int main() {
+
+	Zombie* pointerDirStackZombie = NULL;
+
+	{
+		Zombie stackZombie = Zombie("stackZombie");
+		pointerDirStackZombie = &stackZombie; // keep the direction of the object in stack
+		stackZombie.announce(); // Valid call within scope
+	} // Zombie is destroyed here
+
+	// Undefined behavior: trying to use stackZombie pointer after it goes out of scope
+	std::cout << "Undefined behavior: trying to use stackZombie pointer after it goes out of scope" << std::endl;
+	pointerDirStackZombie->announce();
+	std::cout << std::endl;
+}
+*/
