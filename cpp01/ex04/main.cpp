528d8da3371269c498f99469124f161b94561c24
Amandine <amandined.plateforme@gmail.com>
2025-09-16 10:19:11 +0200

    CPP01-ex04 done - TODO: TESTING

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -5,7 +5,7 @@
 // Replace all occurrences of s1 with s2 in content
 std::string replaceAll(const std::string &content, const std::string &s1, const std::string &s2) {
     if (s1.empty()) {
-        std::cerr << "Error: s1 cannot be empty" << std:endl;
+        std::cerr << "Error: s1 cannot be empty" << std::endl;
         return ""; // because it should return a string
     }
 
@@ -25,18 +25,18 @@ std::string replaceAll(const std::string &content, const std::string &s1, const 
 
 int main(int argc, char **argv) {
     if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std:endl;
+        std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
         return 1;
     }
 
     std::string filename = argv[1];
     std::string s1 = argv[2];
-    std::string s1 = argv[3];
+    std::string s2 = argv[3];
 
     // Open input file
     std::ifstream infile(filename.c_str()); // c_str(): function that returns a C-style string (const char)* version of a C++ std::string (necessary for if/ofstream classes)
     if (!infile) {
-        std::cerr << "Error: could not open input file" << std:endl;
+        std::cerr << "Error: could not open input file" << std::endl;
         return 1;
     }
 
@@ -56,7 +56,7 @@ int main(int argc, char **argv) {
     // Write output file
     std::ofstream outfile((filename + ".replace").c_str()); // Open output file stream
     if (!outfile) {
-        std::cerr << "Error: could not open output file" << std:endl;
+        std::cerr << "Error: could not open output file" << std::endl;
         return 1; 
     }
     outfile << replaced; // Write in the Open output file stream
