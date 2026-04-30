fc29a5ea1e867c45112c0d4d7167c853d1dd4a0d
calamitymandy <amandined.plateforme@gmail.com>
2026-04-30 18:11:44 +0200

    going on with cpp08 ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -3,12 +3,17 @@
 
 #include <stack>
 
-/*std::stack is an adapter, not a real container.
+/*
+std::stack is an adapter, not a real container.
 Internally it contains a protected member:
 container_type c;
 That’s the underlying container (by default: std::deque<T>)
+So the whole point is basically to expose iterators of the underlying container
+and inheritance gives us access to the underlying container of std::stack
 
-So the whole point is basically to expose iterators of the underlying container*/
+std::stack doesn’t provide iterators because it's designed as a restricted interface (LIFO only).
+Iterators would break that abstraction.
+*/
 
 template <typename T>
 class MutantStack : public std::stack<T> {
@@ -20,7 +25,26 @@ class MutantStack : public std::stack<T> {
             return *this;
         }
         ~MutantStack() {}
-};
 
+        /* Creating aliases for the iterators by accessing the iterators within the 
+        underlying container type (deque by default) */
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::reverse_const_iterator reverse_const_iterator;
+
+        // Iterators & const iterators
+        iterator begin() { return this->c.begin(); }
+        iterator end() { return this->c.end(); }
+        const_iterator begin() const { return this->c.begin(); }
+        const_iterator end() const { return this->c.end(); }
+
+        // Reverse iterators & const reverse iterators
+        reverse_iterator rbegin() { return this->c.rbegin(); }
+        reverse_iterator rend() { return this->c.rend(); }
+        reverse_const_iterator rbegin() const { return this->c.rbegin(); }
+        reverse_const_iterator rend() const { return this->c.rend(); }
+};
 
 #endif
